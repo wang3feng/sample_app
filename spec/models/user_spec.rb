a8422a5df3ea5fc67557e5db7dec68559e9c8936
wang3feng <wangfeng.w@gmail.com>
2013-09-23 21:40:32 +0800

    Finish sign in and out

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,8 @@ describe User do
   it { should respond_to(:password_digest)}
   it { should respond_to(:password)}
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
   it { should be_valid}
 
   describe "When user is not present" do
@@ -87,6 +89,11 @@ describe User do
   describe " with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a"*5 }
     it { should be_invalid}
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 
 end
